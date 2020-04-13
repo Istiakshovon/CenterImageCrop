e6500c80c8d22824c4482066b1445f06093d5381
Istiakshovon <Istiakshovon001@gmail.com>
2020-04-13 22:27:46 +0600

    Initial commit

---
+++ app/src/main/java/com/istiak/cropimagetocenter/MainActivity.java
@@ -0,0 +1,167 @@
+package com.istiak.cropimagetocenter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+
+import in.mayanknagwanshi.imagepicker.ImageSelectActivity;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txt;
+    Button btnAudio,btnVideo,btnFile,btnImg,btnUpload;
+    ImageView imgView;
+    ProgressDialog progressDialog;
+
+    String mediaPath, mediaPath1;
+    String[] mediaColumns = {MediaStore.Video.Media._ID};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Uploading.....");
+
+        txt = findViewById(R.id.txt);
+        btnAudio = findViewById(R.id.btn_audio);
+        btnVideo = findViewById(R.id.btn_video);
+        btnFile = findViewById(R.id.btn_file);
+        btnImg = findViewById(R.id.btn_img);
+        imgView = findViewById(R.id.imageView);
+        btnUpload = findViewById(R.id.btn_submit);
+
+
+        btnImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ImageSelectActivity.class);
+                intent.putExtra(ImageSelectActivity.FLAG_COMPRESS, false);//default is true
+                intent.putExtra(ImageSelectActivity.FLAG_CAMERA, true);//default is true
+                intent.putExtra(ImageSelectActivity.FLAG_GALLERY, true);//default is true
+                startActivityForResult(intent, 1213);
+            }
+        });
+        btnAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, 2);
+            }
+        });
+        btnVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, 1);
+            }
+        });
+        btnFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+
+                intent.setType("application/pdf");
+
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(intent, "Select Pdf"), 3);
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+
+
+
+
+            // When an Image is picked
+            if (requestCode == 1213 && resultCode == RESULT_OK && null != data) {
+
+
+                mediaPath = data.getStringExtra(ImageSelectActivity.RESULT_FILE_PATH);
+                Bitmap selectedImage = BitmapFactory.decodeFile(mediaPath);
+//                imgView.setImageBitmap(selectedImage);
+                txt.setText(mediaPath);
+                getCroppedBitmap(selectedImage);
+            }
+
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    // Providing Thumbnail For Selected Image
+    public Bitmap getThumbnailPathForLocalFile(Activity context, Uri fileUri) {
+        long fileId = getFileId(context, fileUri);
+        return MediaStore.Video.Thumbnails.getThumbnail(context.getContentResolver(),
+                fileId, MediaStore.Video.Thumbnails.MICRO_KIND, null);
+    }
+
+    // Getting Selected File ID
+    public long getFileId(Activity context, Uri fileUri) {
+        Cursor cursor = context.managedQuery(fileUri, mediaColumns, null, null, null);
+        if (cursor.moveToFirst()) {
+            int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Video.Media._ID);
+            return cursor.getInt(columnIndex);
+        }
+        return 0;
+    }
+
+
+
+
+    public Bitmap getCroppedBitmap(Bitmap bitmap) {
+
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        // canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
+                bitmap.getWidth() / 2, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        imgView.setImageBitmap(output);
+        return output;
+    }
+}
